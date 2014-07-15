90fb1cb753ec3252f45095f6b638b8b54f565ed4
dlsso <supertonesfan@hotmail.com>
2014-07-15 16:30:25 -0600

    Created CSS scaling board and navbar

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+var resize = function(){
+	$('.board').css('font-size', $('.square').height()*1.06)
+	$('.square').css('line-height', -0.35+0.85*parseInt($('.board').css('font-size'))+'px')
+}
+
+$(window).on('resize', function() {
+	resize();
+})
+
 $(document).on('ready', function() {
-  
+	resize();
 });
