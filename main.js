dd7e7a17ec98bee1973cf3d7f0bb9c12ecdb8e90
dlsso <supertonesfan@hotmail.com>
2014-07-16 18:24:12 -0600

    Really dumb AI implemented

--- main.js
+++ main.js
@@ -17,14 +17,80 @@ var ticTacToe = (function() {
 	}
 
 	// Player constructor
-	var Player = function(name, type) {
-		this.name = name;
-		this.type = type;
+	var Player = function(name, type, ai, marker) {
+		this.name = name
+		this.type = type
+
+		if(ai === "easyComputer"){
+			this.ai = easyComputer
+		}
+
+		if(ai === "hardComputer"){
+			this.ai = hardComputer
+		}
+
+		this.marker = marker
 	}
 
+	//	AI constructor
+	//	================
+	var Computer = function() {
+
+		this.markChoice = function(choice, marker){
+			console.log("choice was "+choice)
+			cells.splice( choice, 1, marker )
+			console.log(cells)
+
+			$('.square[data-cell='+choice+']').html(marker)
+		}
+
+		this.move = function(board, marker, nextPlayer){
+			
+			this.markChoice(this.choose(), marker)
+			if(checkWin()){ disableBoard() }
+			else if(checkTie()){ disableBoard() }
+			else{
+				playerTurn = nextPlayer.name
+				updateReadout(playerTurn + "\'s turn.")
+			}
+		}
+	}
+
+
+	//	Easy AI
+	//	================
+	var EasyComputer = function(){
+
+		// Easy computer logic
+		this.choose = function(board) {
+			var choiceOrder = [4, 0, 8, 2, 6, 1, 5, 3, 7]
+			for (var i = 0; i < choiceOrder.length; i++) {
+				console.log(choiceOrder[i])
+				if(!cells[choiceOrder[i]]){ return choiceOrder[i]}
+			}
+		}
+	}
+	EasyComputer.prototype = new Computer()
+	EasyComputer.prototype.constructor = EasyComputer
+
+
+	//	Hard AI
+	//	================
+	var HardComputer = function(){
+
+		// Hard computer logic
+		this.choose = function() {
+			
+		}
+	}
+	HardComputer.prototype = new Computer()
+	HardComputer.prototype.constructor = HardComputer
+
+
+	// Starts a new name: gets player name and type, resets board, attaches click handler
 	var startGame = function(){
-		player1 = new Player($('#player1-name').html(), $('#player1').val())
-		player2 = new Player($('#player2-name').html(), $('#player2').val())
+		player1 = new Player($('#player1-name').html(), $('#player1').val(), $('#player1').find(':selected').data('ai'), "×")
+		player2 = new Player($('#player2-name').html(), $('#player2').val(), $('#player2').find(':selected').data('ai'), "○")
 		// $('#player1-name').attr('contenteditable','false');
 		// $('#player2-name').attr('contenteditable','false');
 		$('#start').text("New game!")
@@ -68,16 +134,18 @@ var ticTacToe = (function() {
 		$('.square').off('click')
 	}
 
-	var playerMove = function(player, nextPlayer, marker) {
+	var playerMove = function(player, nextPlayer, playerMarker) {
 		if($(this).html() === ""){
-			cells.splice($(this).data('cell'), 1, marker)
-			$(this).html(marker)
+			cells.splice($(this).data('cell'), 1, playerMarker)
+			$(this).html(playerMarker)
+			console.log(cells)
 			
 			if(checkWin()){ disableBoard() }
 			else if(checkTie()){ disableBoard() }
 			else{
 				playerTurn = nextPlayer.name
 				updateReadout(nextPlayer.name + "\'s turn.")
+				if(nextPlayer.type !== "Human"){ nextPlayer.ai.move(cells, nextPlayer.marker, player) }
 			}
 		}
 		else{updateReadout("You can't go there.")}
@@ -85,11 +153,11 @@ var ticTacToe = (function() {
 
 	var placeMarker = function () {
 		if(playerTurn === player1.name){
-			playerMove.call(this, player1, player2, "×")
+			playerMove.call(this, player1, player2, player1.marker)
 		}
 
 		else if(playerTurn === player2.name){
-			playerMove.call(this, player2, player1, "○")
+			playerMove.call(this, player2, player1, player2.marker)
 		}
 
 		else{updateReadout("Start a game first!")}
@@ -102,12 +170,9 @@ var ticTacToe = (function() {
 	var playerTurn = {}
 	var player1 = new Player()
 	var player2 = new Player()
+	var easyComputer = new EasyComputer()
+	var hardComputer = new HardComputer()
 	var cells = []
-
-	var easyComputer = function() {
-
-	}
-
 
 
 	//==========================================================//
